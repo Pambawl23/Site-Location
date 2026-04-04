3f77d775c657ddb9abf4a38226f1f5e8e447beae
Pambawl23 <bapapambaye4@gmail.com>
2026-04-04 01:54:31 +0000

    Update images_voitures.tsx

--- src/images_voitures.tsx
+++ src/images_voitures.tsx
@@ -1,9 +1,12 @@
+import { img } from './imagePath'
+
+
 export const cars = [
 {
   id: 'toyota-rav4',
   name: 'Toyota RAV4',
   image:
-  <img src="/images/Gmail/IMG_3456.jpeg" />,
+  { src: img("/images/Gmail/IMG_3456.jpeg")},
   desc: 'Berline confortable et économique, idéale pour les trajets urbains et interurbains.',
   priceDay: '35 000 FCFA',
   seats: 5,
@@ -14,7 +17,7 @@ export const cars = [
   id: 'toyota-rav4-noir',
   name: 'Toyota RAV4 Noir',
   image:
-  <img src="/images/Gmail/IMG_3457.webp" />,
+  {src : img("/images/Gmail/IMG_3457.webp")},
   desc: 'SUV robuste et polyvalent, parfait pour affronter tous types de routes au Sénégal.',
   priceDay: '35 000 FCFA',
   seats: 5,
@@ -25,7 +28,7 @@ export const cars = [
   id: 'avensis-break',
   name: 'Avensis Break',
   image:
-  <img src="/images/Gmail/IMG_3462.jpeg" />,
+  { src: img("/images/Gmail/IMG_3462.jpeg")},
   desc: 'Offrant un confort exceptionnel pour vos voyages.',
   priceDay: '30 000 FCFA',
   seats: 5,
@@ -36,7 +39,7 @@ export const cars = [
   id: 'avensis-break-royal',
   name: 'Avensis Break Royal',
   image:
-  <img src="/images/Gmail/IMG_3463.jpeg" />,
+  { src : {img("/images/Gmail/IMG_3463.jpeg")},
   desc: 'Berline de luxe pour vos déplacements professionnels ou événements spéciaux.',
   priceDay: '30 000 FCFA',
   seats: 5,
@@ -47,7 +50,7 @@ export const cars = [
   id: 'toyota-rav4-blanc',
   name: 'Toyota RAV4 Blanc',
   image:
-  <img src="/images/Gmail/IMG_3467.jpeg" />,
+  { src: img("/images/Gmail/IMG_3467.jpeg")},
   desc: 'SUV élégant et spacieux, idéal pour les familles ou les groupes d\'amis en voyage.',
   priceDay: '35 000 FCFA',
   seats: 5,
