ceda5b20d9fcae32191395ecf741680e58ea54c9
Pambawl23 <bapapambaye4@gmail.com>
2026-04-04 01:36:45 +0000

    Update base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Site-Location/',
+  base: process.env.VERCEL ? '/' : '/Site-Location/',
   plugins: [react()],
 })
